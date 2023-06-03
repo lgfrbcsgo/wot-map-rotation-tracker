bd6697f1c9f24b4db0e8d16a5174f346f8e9b333
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2023-06-03 21:50:29 +0200

    Publish blocked maps

--- src/mod_map_rotation_tracker/server.py
+++ src/mod_map_rotation_tracker/server.py
@@ -1,16 +1,19 @@
 import json
 import re
-from typing import Optional
+from typing import Optional, List
 
 import BigWorld
+
+from ArenaType import g_geometryCache
+from PlayerEvents import g_playerEvents
 from constants import ARENA_BONUS_TYPE
 from debug_utils import LOG_NOTE
 from helpers import dependency
 from mod_async import CallbackCancelled, async_task, await_event, delay
 from mod_async_server import Server
 from mod_websocket_server import MessageStream, websocket_protocol
-from PlayerEvents import g_playerEvents
 from skeletons.connection_mgr import IConnectionManager
+from skeletons.gui.lobby_context import ILobbyContext
 
 PORT = 15457
 
@@ -19,17 +22,21 @@ ORIGIN_WHITELIST = [
     "https://lgfrbcsgo.github.io",
 ]
 
-
-connection_manager = dependency.instance(IConnectionManager)
-
-
 class Listener(object):
     def __init__(self):
         self._stream = None  # type: Optional[MessageStream]
+        self._blocked_maps = None # type: Optional[List]
 
     @async_task
     def on_connect(self, stream):
         # type: (MessageStream) -> ...
+        if self._blocked_maps is not None:
+            message = json.dumps({
+                "type": "BlockedMaps",
+                "maps": self._blocked_maps
+            })
+            yield stream.send_message(message)
+
         previous, self._stream = self._stream, stream
         if previous:
             yield previous.close(code=4000, reason="ConnectionSuperseded")
@@ -40,7 +47,35 @@ class Listener(object):
             self._stream = None
 
     @async_task
-    def on_arena_load(self):
+    @dependency.replace_none_kwargs(lobby_context=ILobbyContext)
+    def on_state_update(self, diff, lobby_context=None):
+        blocked_maps_update = diff.get("preferredMaps", {}).get("blackList")
+        if blocked_maps_update is None:
+            return
+
+        config = lobby_context.getServerSettings().getPreferredMapsConfig()
+        cooldown = config["slotCooldown"]
+
+        self._blocked_maps = [
+            {
+                "map": g_geometryCache[map_id].geometryName,
+                "blocked_until": blocked_at + cooldown
+            }
+            for (map_id, blocked_at) in blocked_maps_update
+            if map_id != 0
+        ]
+
+        message = json.dumps({
+            "type": "BlockedMaps",
+            "maps": self._blocked_maps
+        })
+
+        if self._stream is not None:
+            yield self._stream.send_message(message)
+
+    @async_task
+    @dependency.replace_none_kwargs(connection_manager=IConnectionManager)
+    def on_arena_load(self, connection_manager=None):
         arena = BigWorld.player().arena
 
         if arena.bonusType != ARENA_BONUS_TYPE.REGULAR:
@@ -53,17 +88,16 @@ class Listener(object):
             for vehicle_info in arena.vehicles.values()
         }
 
-        data = {
+        message = json.dumps({
             "type": "PlayedMap",
             "server": connection_manager.serverUserNameShort,
             "map": arena.arenaType.geometryName,
             "mode": arena.arenaType.gameplayName,
             "bottom_tier": min(tiers),
             "top_tier": max(tiers),
-        }
+        })
 
         if self._stream is not None:
-            message = json.dumps(data)
             yield self._stream.send_message(message)
 
 
@@ -94,7 +128,9 @@ class MapRotationServer(object):
     @async_task
     def serve(self):
         LOG_NOTE("Listening on port {}".format(PORT))
-        g_playerEvents.onAvatarBecomePlayer += self._listener.on_arena_load
+
+        g_playerEvents.onAvatarBecomePlayer += self._on_avatar_become_player
+        g_playerEvents.onClientUpdated += self._on_client_updated
 
         try:
             with self._server as server:
@@ -104,11 +140,18 @@ class MapRotationServer(object):
         except CallbackCancelled:
             pass
         finally:
-            g_playerEvents.onAvatarBecomePlayer -= self._listener.on_arena_load
+            g_playerEvents.onAvatarBecomePlayer -= self._on_avatar_become_player
+            g_playerEvents.onClientUpdated -= self._on_client_updated
             LOG_NOTE("Stopped server")
 
     def close(self):
         self._server.close()
+
+    def _on_avatar_become_player(self, *args, **kwargs):
+        self._listener.on_arena_load()
+
+    def _on_client_updated(self, diff, *args, **kwargs):
+        self._listener.on_state_update(diff)
 
 
 g_map_rotation_server = MapRotationServer()
